19c2721b4e00a6791717e4df709d8f87dcf19f77
percy <percy3991@gmail.com>
2025-07-01 08:53:45 -0500

    feat: se agrego lo trabajado en la clase 3

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("OK")
+from flask import Flask, render_template, abort
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("base.html")
+
+@app.route("/home", methods=["GET"])
+def home():
+    return render_template("home.html")
+
+@app.route("/post", methods=["GET"])
+def get_all_post():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts;').fetchall()
+    return render_template("post/post_list.html", posts=posts)
+
+@app.route("/post/<int:post_id>", methods=["GET"])
+def get_one_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    print(post)
+    if post is None:
+        abort(404)
+    return render_template("post/post.html", post=post)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
