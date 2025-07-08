55a73bc505023d71871d97e8997a18f01ce41968
percy <percy3991@gmail.com>
2025-07-08 09:04:36 -0500

    proyecto finalizado

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, request, redirect, url_for
 import sqlite3
 
 def get_db_connection():
@@ -31,6 +31,49 @@ def get_one_post(post_id):
         abort(404)
     return render_template("post/post.html", post=post)
 
+@app.route('/post/create', methods=['GET', 'POST'])
+def create_one_post():
+    if request.method == "GET":
+        return render_template("post/create.html")
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        conn = get_db_connection()
+        conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('get_all_post'))
+
+
+@app.route('/post/update/<int:post_id>', methods=['GET', 'POST'])
+def update_one_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    
+    if request.method == "GET":
+        return render_template("post/update.html", post=post)
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        conn = get_db_connection()
+        conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, post_id))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('get_all_post'))
+
+@app.route('/post/delete/<int:post_id>', methods=['DELETE'])
+def delete_one_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+
+    if post is None:
+        conn.close()
+        abort(404)
+
+    conn.execute('DELETE FROM posts WHERE id = ?', (post_id,))
+    conn.commit()
+    conn.close()
+    return "", 200
 
 if __name__ == '__main__':
     app.run(debug=True)
